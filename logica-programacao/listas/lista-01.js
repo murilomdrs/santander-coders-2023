341ffdd1da03da3ca5641a136e8ceb3eeaa5a0f7
Murilo Medeiros <murilomdrs@gmail.com>
2023-08-09 22:14:28 -0300

    resolução da lista até o exercício 12

--- logica-programacao/listas/lista-01.js
+++ logica-programacao/listas/lista-01.js
@@ -136,7 +136,22 @@ alert(numeroPrimo(numero));
 // Coerção e Tipos de Dados:
 
 // 11- Declare uma variável com valor "5" (string) e outra variável com valor 2 (número). Multiplique as duas variáveis e explique o resultado.
+
+/*
+const valor1 = '5';
+const valor2 = 2;
+const multiplicacao = valor1 * valor2;
+alert(`O JavaScript é dinamicamente tipado, com isso ele converte a string para número e o resultado da multiplicação entre ${valor1} e ${valor2} é: ${multiplicacao}`);
+*/
+
 // 12- Converta a string "123" em um número inteiro e armazene-o em uma variável.
+
+/*
+sequenciaString = '123';
+converteInteiro = parseInt(sequenciaString);
+alert(`O elemento ${converteInteiro} passado como ${typeof sequenciaString} foi convertido para o tipo ${typeof converteInteiro}`);
+*/
+
 // 13- Escreva um programa que adicione um número e uma string. 
 // 14- Explique o resultado e discuta a coerção que ocorre.
 // 15- Declare uma variável com o valor null e outra variável com o valor undefined. Verifique se são iguais em valor e tipo.
