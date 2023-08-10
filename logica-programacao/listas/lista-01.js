27d3d6840c86a4afccb561f6803c3babdeb05060
Murilo Medeiros <murilomdrs@gmail.com>
2023-08-10 17:01:03 -0300

    finalização e entrega da lista 01

--- logica-programacao/listas/lista-01.js
+++ logica-programacao/listas/lista-01.js
@@ -152,15 +152,135 @@ converteInteiro = parseInt(sequenciaString);
 alert(`O elemento ${converteInteiro} passado como ${typeof sequenciaString} foi convertido para o tipo ${typeof converteInteiro}`);
 */
 
-// 13- Escreva um programa que adicione um número e uma string. 
+// 13- Escreva um programa que adicione um número e uma string.
+
+/*
+const numero = prompt('Insira um número');
+const palavra = prompt('Insira uma frase ou palavra');
+
+const concatenacao = numero + palavra;
+alert(concatenacao);
+*/
+
 // 14- Explique o resultado e discuta a coerção que ocorre.
+
+/*
+alert('Ocorre uma coerção implícita de string pois o compilador atribui automaticamente os tipos de dados. Dessa forma ele concatena as duas variáveis em um único, tipo string');
+*/
+
 // 15- Declare uma variável com o valor null e outra variável com o valor undefined. Verifique se são iguais em valor e tipo.
+
+/*
+const varNull = null;
+const varUndefined = undefined;
+
+alert(varNull === varUndefined ? 'São idênticas em valor e tipo' : 'São diferentes de valor e tipo');
+alert(`valor: ${varNull} - tipo: ${typeof varNull}`);
+alert(`valor: ${varUndefined} - tipo: ${typeof varUndefined}`);
+*/
+
 // 16- Crie um programa que determine se uma variável é do tipo função usando o operador typeof.
+
+/* 
+const estaFuncao = () => {
+  console.log('Achou a função');
+};
+
+alert(estaFuncao);
+alert(typeof estaFuncao);
+ */
 
 // Operadores Matemáticos e Lógicos:
 
 // 18- Escreva um programa que calcule o resultado da expressão: (10 + 5) * 3 / 20.
+
+/*
+const expressao = ((10+5)*3)/20;
+alert(expressao);
+*/
+
 // 19- Crie uma função que verifique se um número é positivo e ímpar ao mesmo tempo.
+
+/*
+function numeroPositivoImpar(numero) {
+  if(numero >= 0 && numero %2 != 0) {
+    return alert(`O número ${numero} é positivo e ímpar`);
+  } else if(numero >= 0 && numero %2 == 0) {
+    return alert(`O número ${numero} é positivo e par`);
+  } else {
+    return alert(`O número ${numero} é negativo`);
+  }  
+}
+
+const numero = numeroPositivoImpar(prompt('Insira um número qualquer'));
+*/
+
+
 // 20- Implemente um programa que determine se um ano fornecido pelo usuário é bissexto ou não, considerando as regras: (a) Anos divisíveis por 4 são bissextos; (b) Anos divisíveis por 100 não são bissextos, a menos que também sejam divisíveis por 400.
+
+/*
+function checarBissexto(ano) {
+  if(ano % 4 == 0) {
+    if(ano % 100 == 0) {
+      if(ano % 400 == 0) {
+        return `O ano ${ano} é um ano bissexto.`;
+      } else {
+        return `O ano ${ano} não é um ano bissexto.`
+      }
+    } else {
+      return `O ano ${ano} é um ano bissexto.`;
+    }
+  } else {
+    return `O ano ${ano} não é um ano bissexto.`
+  }
+}
+
+const anoBuscado = prompt('Insira um ano no formato AAAA');
+alert(checarBissexto(anoBuscado));
+*/
+
 // 21- Escreva uma função que determine se uma pessoa tem permissão para entrar em um clube: idade mínima de 18 anos e não estar na lista de membros banidos (use operadores lógicos).
+
+/*
+function permissaoEntrada(nome, idade) {
+  if(idade >= 18) {
+    if(membros.includes(nome)) {
+      return `${nome}, você está banido deste clube.`;
+    } else {
+      return `Olá ${nome}, boas vindas.`;
+    }
+  } else {
+    return 'Hoje não, Faro!';
+  }
+}
+
+let membros = ['Pedro', 'Rodrigo', 'Fernando'];
+const idadeMinima = 18;
+
+const nome = prompt('Informe seu nome');
+const idade = prompt('Informe sua idade');
+alert(permissaoEntrada(nome, idade));
+*/
+
 // 22- Crie um programa que simule um sistema de login. Peça ao usuário um nome de usuário e uma senha, e verifique se correspondem aos valores esperados.
+
+/*
+function consultaBanco(usuario, senha) {
+  for(let dados of bancoDados) {
+    if(dados.nome === usuario && dados.senha === senha) {
+      return 'Login realizado com sucesso';
+    }
+  }
+  return 'Usuário e senha incorretos ou sem cadastro.'  
+}
+
+const bancoDados = [
+  {nome: 'pedro', senha: 'pedro123'},
+  {nome: 'jose', senha: 'jose123'},
+  {nome: 'joao', senha: 'joao123'}
+];
+
+const usuario = prompt('Informe seu usuario:');
+const senha = prompt('Informe sua senha:');
+alert(consultaBanco(usuario, senha));
+*/
